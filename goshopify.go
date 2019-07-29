9d25a99fb3debd3622c28d39d99cd47b5b6f96c8
Kaniel Yano <gir@users.noreply.github.com>
2019-07-29 08:59:31 -0700

    Revert all throttling and retry logic (#30)
    * Revert "Merge pull request #28 from myhelix/mhl-rate-limit-backoff"
    
    This reverts commit df168687af9a8d12ca905624d23677eb1db57ff8, reversing
    changes made to 2a4c4a2b1a42097f54e7647b9382802a0b60eef9.
    
    * Revert "Merge pull request #27 from myhelix/hdo-689-gs-ignore-reported-error"
    
    This reverts commit 2a4c4a2b1a42097f54e7647b9382802a0b60eef9, reversing
    changes made to eb9c545f40e9304ff10fc1a852874ac89a883ad1.
    
    * Revert "Merge pull request #26 from myhelix/hdo-689-gs-distributed-api-usage"
    
    This reverts commit eb9c545f40e9304ff10fc1a852874ac89a883ad1, reversing
    changes made to ba4fcc04ceb09afd6310946ed9e1178e87feb23c.

--- goshopify.go
+++ goshopify.go
@@ -5,7 +5,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/eapache/go-resiliency/retrier"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -19,22 +18,19 @@ import (
 )
 
 const (
-	UserAgent       = "goshopify/1.0.0"
+	UserAgent = "goshopify/1.0.0"
+
 	callLimitHeader = "X-Shopify-Shop-Api-Call-Limit"
 )
-
-var bucketCapacity = 0
 
 // App represents basic app settings such as Api key, secret, scope, and redirect url.
 // See oauth.go for OAuth related helper functions.
 type App struct {
-	ApiKey          string
-	ApiSecret       string
-	RedirectUrl     string
-	Scope           string
-	Password        string
-	BucketThreshold int           // when bucket capacity reaches threshold delay next request
-	HeavyLoadDelay  time.Duration // delay between requests under heavy load
+	ApiKey      string
+	ApiSecret   string
+	RedirectUrl string
+	Scope       string
+	Password    string
 }
 
 // Client manages communication with the Shopify API.
@@ -226,11 +222,6 @@ func (c *Client) WithLogger(l Logger) *Client {
 // response. It does not make much sense to call Do without a prepared
 // interface instance.
 func (c *Client) Do(req *http.Request, v interface{}) error {
-	// Self-throttle if bucket capacity is specified and exceeds threshold
-	if c.app.BucketThreshold > 0 && bucketCapacity > c.app.BucketThreshold {
-		c.log.Warn("sleep due to heavy Shopify API traffic for time=%s.", c.app.HeavyLoadDelay)
-		time.Sleep(c.app.HeavyLoadDelay)
-	}
 	resp, err := c.Client.Do(req)
 	if err != nil {
 		return err
@@ -243,16 +234,7 @@ func (c *Client) Do(req *http.Request, v interface{}) error {
 	}
 
 	if callLimit, ok := resp.Header[callLimitHeader]; ok {
-		logMessage := fmt.Sprintf("%s: %s", callLimitHeader, callLimit[0])
-		// Extract the current bucket capacity from the call limit response header
-		bucketCapacity, err = strconv.Atoi(strings.Split(callLimit[0], "/")[0])
-		if err != nil {
-			logMessage = "Unable to parse the call limit header: " + logMessage
-			// set bucket capacity to the threshold + 1 to throttle requests until call limit header can be parsed
-			bucketCapacity = c.app.BucketThreshold + 1
-		}
-
-		c.log.Info(logMessage)
+		c.log.Info("%s: %s", callLimitHeader, callLimit[0])
 	}
 
 	if v != nil {
@@ -264,40 +246,6 @@ func (c *Client) Do(req *http.Request, v interface{}) error {
 	}
 
 	return nil
-}
-
-var errorClassifier ErrorClassifier
-
-type ErrorClassifier struct{}
-
-func (s ErrorClassifier) Classify(err error) retrier.Action {
-	if err == nil {
-		return retrier.Succeed
-	}
-
-	switch err.(type) {
-	case RateLimitError:
-		return retrier.Retry
-
-	default:
-		return retrier.Fail
-	}
-}
-
-func (c *Client) DoWithRetry(req *http.Request, v interface{}) (err error) {
-	// exponential retry 5 times with a maximum of 15 seconds delay
-	r := retrier.New(retrier.ExponentialBackoff(4, 1*time.Second), errorClassifier)
-	numRetry := 0
-	err = r.Run(func() error {
-		err := c.Do(req, v)
-		numRetry += 1
-		if err != nil {
-			c.log.Warn("error occurred calling Shopify: %v; numCalled=%d; may retry", err, numRetry)
-		}
-		return err
-	})
-
-	return err
 }
 
 func wrapSpecificError(r *http.Response, err ResponseError) error {
@@ -445,7 +393,7 @@ func (c *Client) CreateAndDo(method, path string, data, options, resource interf
 		return err
 	}
 
-	err = c.DoWithRetry(req, resource)
+	err = c.Do(req, resource)
 	if err != nil {
 		return err
 	}
